7359b4b58c8a5f8be03b0d366bb07c92e4edd040
Gopal Krishna Panda <gopal.krishna@aol.in>
2025-08-31 23:29:16 +0530

    Add files via upload

--- predictive_m.py
+++ predictive_m.py
@@ -28,24 +28,26 @@ type_mapping = {"H": 0, "L": 1, "M": 2}
 Type_encoded = type_mapping[Type]
 
 Air_Temperature = st.number_input('Air_Temperature', 2.0, 4.5, 3.0) 
-st.write("Air_Temperature:", Air_Temperature)
+st.write("Air_Temperature:", Air_Temperature,format="%.2f")
 
 Process_Temperature = st.number_input('Process_Temperature', min_value=2.0,max_value=4.5,value=3.0,step=0.1,format="%.2f") 
-st.write("Process_Temperature:", Process_Temperature)
+st.write("Process_Temperature:", Process_Temperature,format="%.2f")
 
 Roational_Temperature = st.number_input('Roational_Temperature', min_value=2.0,max_value=4.5,value=3.0,step=0.1,format="%.2f") 
-st.write("Roaational_Temperature:", Roational_Temperature)
+st.write("Roational_Temperature:", Roational_Temperature,format="%.2f")
 
 Torque_Temperature = st.number_input('Torque_Temperature', min_value=2.0,max_value=4.5,value=3.0,step=0.1,format="%.2f") 
-st.write("Torque_Temperature:", Torque_Temperature)
+st.write("Torque_Temperature:", Torque_Temperature,format="%.2f")
 
 Tool_Wear_Min = st.number_input('Tool_Wear_Min', min_value=2.0,max_value=4.5,value=3.0,step=0.1,format="%.2f") 
-st.write("Tool_Wear_Min:", Tool_Wear_Min)
+st.write("Tool_Wear_Min:", Tool_Wear_Min,format="%.2f")
 
 # Predict button
 if st.button('Predict Binary Classification'):
     input_data = np.array([[Type_encoded, Air_Temperature, Process_Temperature, Roational_Temperature, Torque_Temperature, Tool_Wear_Min]])
     model = model_bin
+    if(model_bin is None):
+        st.error("Model not loaded. Please load the model first.")
     prediction = model.predict(input_data)
     st.success(f'Predicted Target: **{Target[prediction[0]]}**')
         
@@ -53,6 +55,8 @@ if st.button('Predict Binary Classification'):
 if st.button('Predict - Multi Classification'):
     input_data = np.array([[Type_encoded, Air_Temperature, Process_Temperature, Roational_Temperature, Torque_Temperature, Tool_Wear_Min]])   
     model = model_multi
+    if(model_multi is None):
+        st.error("Model not loaded. Please load the model first.")
     prediction = model.predict(input_data)
     st.success(f'Predicted Failure_Type: **{FailureType[prediction[0]]}**')
     
